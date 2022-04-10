b3d628dfb4b13c3dc76483a8f7de055da77b08b3
Ryos Suzuki <ryos@sinby.com>
2022-04-10 17:32:27 +0900

    t13_ryos.py 適当に動いた

--- pycairo-examples/t13_ryos.py
+++ pycairo-examples/t13_ryos.py
@@ -20,10 +20,10 @@ def set_source_color(context, name):
 
 
 #----------------------------------------------------------------
-def draw_label(ctx, x, y, num, str):
-    print(x, y, 'に移動して')
-    print(num, 'を描画')
-    print(str, 'を描画')
+def draw_label(ctx, my_x, my_y, my_num, my_str):
+    print(my_x, my_y, 'に移動して')
+    print(my_num, 'を描画')
+    print(my_str, 'を描画')
 
     x = 40
     y = 50
@@ -32,61 +32,58 @@ def draw_label(ctx, x, y, num, str):
     r = 20
 
     line_width = 20
-    context.set_line_width(line_width)
+    ctx.set_line_width(line_width)
 
-    context.move_to(x+r, y)
-    context.line_to(x+w-r-1, y)
-    context.arc(x+w-r-1, y+r, r, -0.5*math.pi, 0)
-    context.line_to(x+w-1, y+h-r-1)
-    context.arc(x+w-r-1, y+h-r-1, r, 0, 0.5*math.pi)
-    context.line_to(x+r, y+h-1)
-    context.arc(x+r, y+h-r-1, r, 0.5*math.pi, math.pi)
-    context.line_to(x, y+r)
-    context.arc(x+r, y+r, r, math.pi, 1.5*math.pi)
-    context.close_path()
+    ctx.move_to(x+r, y)
+    ctx.line_to(x+w-r-1, y)
+    ctx.arc(x+w-r-1, y+r, r, -0.5*math.pi, 0)
+    ctx.line_to(x+w-1, y+h-r-1)
+    ctx.arc(x+w-r-1, y+h-r-1, r, 0, 0.5*math.pi)
+    ctx.line_to(x+r, y+h-1)
+    ctx.arc(x+r, y+h-r-1, r, 0.5*math.pi, math.pi)
+    ctx.line_to(x, y+r)
+    ctx.arc(x+r, y+r, r, math.pi, 1.5*math.pi)
+    ctx.close_path()
 
-    #context.set_source_rgb(0xff/float(0xff), 0xff/float(0xff), 0xff/float(0xff))
-    set_source_color(context, 'tomato')
-    context.fill_preserve()
+    #ctx.set_source_rgb(0xff/float(0xff), 0xff/float(0xff), 0xff/float(0xff))
+    set_source_color(ctx, 'tomato')
+    ctx.fill_preserve()
 
-    #context.set_source_rgb(0xff/float(0xff), 0x99/float(0xff), 0x00/float(0xff))
-    #set_source_color(context, 'seagreen')
-    set_source_color(context, 'blueviolet')
-    #set_source_color(context, 'c')
-    context.stroke()
+    #ctx.set_source_rgb(0xff/float(0xff), 0x99/float(0xff), 0x00/float(0xff))
+    #set_source_color(ctx, 'seagreen')
+    set_source_color(ctx, 'blueviolet')
+    #set_source_color(ctx, 'c')
+    ctx.stroke()
 
     x = 75
     y = 95
 
-    context.set_source_rgb(0, 0, 0)
+    ctx.set_source_rgb(0, 0, 0)
     r = 20
     line_width = 3
-    context.set_line_width(line_width)
-    context.arc(x, y, r, 0, math.pi * 2)
-    context.stroke()
+    ctx.set_line_width(line_width)
+    ctx.arc(x, y, r, 0, math.pi * 2)
+    ctx.stroke()
 
-    str = '1'
     font_size = 30
     sf = scaled_font.get_scaled_font('Arial', font_size )
-    extents = sf.text_extents(str)
-    context.set_source_rgb(0, 0, 0)
-    context.set_font_size(font_size)
-    context.move_to(x, y)
-    context.rel_move_to(-extents.width/2, -extents.height/2)
-    context.rel_move_to(-extents.x_bearing, -extents.y_bearing)
-    context.show_text(str)
-    context.stroke()
+    extents = sf.text_extents(my_num)
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.set_font_size(font_size)
+    ctx.move_to(x, y)
+    ctx.rel_move_to(-extents.width/2, -extents.height/2)
+    ctx.rel_move_to(-extents.x_bearing, -extents.y_bearing)
+    ctx.show_text(my_num)
+    ctx.stroke()
 
-    context.select_font_face('Ricty Diminished', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    x = 115
-    y = 100
-    str = 'こんにちは'
+    ctx.select_font_face('Ricty Diminished', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+
     font_size = 30
-    context.set_source_rgb(0, 0, 0)
-    context.set_font_size(font_size)
-    context.move_to(x, y)
-    context.show_text('こんにちは')
-    context.stroke()
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.set_font_size(font_size)
+    ctx.move_to(my_x, my_y)
+    ctx.show_text(my_str)
+    ctx.stroke()
 
 #----------------------------------------------------------------
 if __name__ == '__main__':
@@ -98,8 +95,7 @@ if __name__ == '__main__':
 
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
     context = cairo.Context(surface)
-    ctx = None
 
-    draw_label(ctx, 0, 0, '十', 'Hello')
+    draw_label(context, 115, 100, '十', 'Hello')
 
     surface.write_to_png(image_file)

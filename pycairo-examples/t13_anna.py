22495b8f1b7746928727675664145730f890426e
AnnaGG4strn0m3r <annag7@hawaii.edu>
2022-04-16 22:30:46 -1000

    Complete circle and number at left

--- pycairo-examples/t13_anna.py
+++ pycairo-examples/t13_anna.py
@@ -19,8 +19,8 @@ def set_source_color(context, name):
                 break
 
 def draw_frame(ctx, x, y, w, h, r):
-    # outside purple frame
-    print("draw photoframe", x, y, w, h, r)
+    # outside purple frame and background 
+    print("draw outside photoframe", x, y, w, h, r)
     line_width = 20
     ctx.set_line_width(line_width)
 
@@ -45,6 +45,29 @@ def draw_frame(ctx, x, y, w, h, r):
     #set_source_color(ctx, 'c')
     ctx.stroke()
 
+def draw_circle_num(ctx, x, y, r, num, color):
+    # first circle at very left including number inside
+    print('draw circle and put number inside', x, y, r, num, color)
+
+    #ctx.set_source_rgb(0, 0, 0)
+    # drawing circle 
+    ctx.set_source_rgb(color[0], color[1], color[2])
+    line_width = 3
+    ctx.set_line_width(line_width)
+    ctx.arc(x, y, r, 0, math.pi * 2)
+    ctx.stroke()
+
+    # writing number '1'
+    font_size = 30
+    sf = scaled_font.get_scaled_font('Arial', font_size )
+    extents = sf.text_extents(num)
+    ctx.set_font_size(font_size)
+    ctx.move_to(x, y)
+    ctx.rel_move_to(-extents.width/2, -extents.height/2)
+    ctx.rel_move_to(-extents.x_bearing, -extents.y_bearing)
+    ctx.show_text(num)
+    ctx.stroke()
+
 def draw_label(ctx, my_x, my_y, my_num, my_str):
     print('move to', my_x, my_y)
     print('draw', my_num)
@@ -57,30 +80,6 @@ def draw_label(ctx, my_x, my_y, my_num, my_str):
     h = 100
     r = 20
 
-    draw_frame(ctx, x, y, w, h, r)
-
-
-    x = 75
-    y = 95
-
-    ctx.set_source_rgb(0, 0, 0)
-    r = 20
-    line_width = 3
-    ctx.set_line_width(line_width)
-    ctx.arc(x, y, r, 0, math.pi * 2)
-    ctx.stroke()
-
-    str = '1'
-    font_size = 30
-    sf = scaled_font.get_scaled_font('Arial', font_size )
-    extents = sf.text_extents(my_num)
-    ctx.set_source_rgb(0, 0, 0)
-    ctx.set_font_size(font_size)
-    ctx.move_to(x, y)
-    ctx.rel_move_to(-extents.width/2, -extents.height/2)
-    ctx.rel_move_to(-extents.x_bearing, -extents.y_bearing)
-    ctx.show_text(my_num)
-    ctx.stroke()
 
     ctx.select_font_face('Hiragino Sans', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
     print('here0', x, y, str)
@@ -95,6 +94,8 @@ def draw_label(ctx, my_x, my_y, my_num, my_str):
 
 if __name__ == '__main__':
     image_file = 'image_' + sys.argv[0].replace('.py', '.png')
+    image_file1 = 'image_frame_practice.png'
+    image_file2 = 'image_num_practice.png'
     width = 400
     height = 200
     line_width = 10
@@ -103,6 +104,8 @@ if __name__ == '__main__':
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
     context = cairo.Context(surface)
 
-    draw_label(context, 30, 100, '1', 'Hello')
+    #draw_frame(context, 140, 50, 380, 100, 40)
+    draw_circle_num(context, 10, 10, 20, '1', (255, 255, 255))
+    #draw_label(context, 30, 100, '1', 'Hello')
 
-    surface.write_to_png(image_file)
+    surface.write_to_png(image_file2)

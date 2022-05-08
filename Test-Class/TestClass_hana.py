e954b8ab1085436abb27bd368d79be5f5a7ecf99
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2022-05-08 16:55:21 +0900

    追加

--- Test-Class/TestClass_hana.py
+++ Test-Class/TestClass_hana.py
@@ -1,2 +1,12 @@
+from _typeshed import Self
+
+
 class TestClass:
-	pass
+	def __init__(self, name):
+		self.name = name
+
+if __name__ == '__main__':
+	obj1 = TestClass()
+	print(obj1)
+	obj2 = TestClass()
+	print(obj2)

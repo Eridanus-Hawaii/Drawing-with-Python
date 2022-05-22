e11abac44a901ea1bc2f92bbd9058e46d1c3a01b
AnnaGG4strn0m3r <annag7@hawaii.edu>
2022-05-21 21:47:03 -1000

    turle practice

--- Test-Class/class_practice_anna.py
+++ Test-Class/class_practice_anna.py
@@ -1,12 +1,31 @@
 # practice of anna 
 class TestClass:
-    def __init__(self, name):
+    def __init__(self, name, age):
         # __init__ is the rule first creator made so every time we have to foloow
         self.name = name
+        self.age = age
+        self.last_food = 'Nothing'
+
+    def eat(self, food):
+        self.last_food = food
+        print(self.name, 'eats', food)
+
+    def __str__(self):
+        return 'Name:' + self.name + '\n' + 'Age:' + str(self.age) + '\n' + 'Last:' + self.last_food
 
 
 if __name__ == '__main__':
-    obj1 = TestClass()
-    print(obj1)
-    obj2 = TestClass()
-    print(obj2)
+    anna = TestClass('Anna', 23)
+    print(anna)
+    anna.eat('sushi')
+    print(anna)
+    print('----')
+
+    hana = TestClass('Hana', 23)
+    hana.eat('pudding')
+    print(hana)
+    print('----')
+
+    ryos = TestClass('ryos', 57)
+    ryos.eat('spaghetti')
+    print(ryos)

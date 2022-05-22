1d31004a93b1647a2c35892ae9272ff028466e09
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2022-05-22 17:26:09 +0900

    hana追加

--- Test-Class/TestClass_hana.py
+++ Test-Class/TestClass_hana.py
@@ -1,9 +1,30 @@
 class TestClass:
-	def __init__(self, name):
+	def __init__(self, name, age):
 		self.name = name
+		self.age = age
+		self.last_food = 'Nothing'
+	
+	def eat(self, food):
+		self.last_food = food
+		print(self.name, 'eats', food)
+
+	def __str__(self):
+		return 'Name:' + self.name + 'Age:' + str(self.age) + 'Last:' + self.last_food
 
 if __name__ == '__main__':
-	obj1 = TestClass()
-	print(obj1)
-	obj2 = TestClass()
-	print(obj2)
+	hana = TestClass("Hana", 23)
+	print('-----------')
+	print(hana)
+	hana.eat('プリン')
+	print(hana)
+	print('-----------')
+
+	anna = TestClass("Anna", 23)
+	anna.eat('Sushi')
+	print(anna)
+	print('-----------')
+
+	ryos = TestClass("ryos", 57)
+	ryos.eat('スパゲッティ')
+	print(ryos)
+	print('-----------')

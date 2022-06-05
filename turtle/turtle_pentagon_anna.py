472c141e530445e326ffd54c8dcc372bbcbe6e7e
AnnaGG4strn0m3r <annag7@hawaii.edu>
2022-06-04 21:35:47 -1000

    new version

--- turtle/turtle_pentagon_anna.py
+++ turtle/turtle_pentagon_anna.py
@@ -1,1 +1,16 @@
 import turtle 
+import numpy as np 
+import math
+obj = turtle.Turtle()
+def polygon(t, n, r):
+    #theta = 180 - (180 - (360 / n))
+    theta = 360 / n
+    step = r * 2 * np.sin(np.pi/n)
+    for i in range(n):
+        t.right(theta)
+        t.forward(step)
+
+polygon(obj, 5, 100)
+polygon(obj, 98, 100)
+screen = turtle.Screen()
+screen.exitonclick()

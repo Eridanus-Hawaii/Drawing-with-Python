ce9874cc5319ff30b1fc18ff458fa4f251d5223c
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2022-06-05 16:35:06 +0900

    変更

--- turtle/turtle_pentagon_hana.py
+++ turtle/turtle_pentagon_hana.py
@@ -1,1 +1,19 @@
 import turtle 
+import math
+
+obj = turtle.Turtle()
+def polygon(t, n, r):
+    theta = (360 / n)
+    step = r * 2 * math.sin(math.pi / n)
+    for i in range(n):
+        t.right(theta)
+        t.forward(step)
+
+polygon(obj, 99, 100)
+
+polygon(obj, 5, 100)
+
+polygon(obj, 9, 100)
+
+screen = turtle.Screen()
+screen.exitonclick()

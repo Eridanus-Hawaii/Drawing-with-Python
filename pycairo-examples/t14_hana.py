3ab0c9b4a95af75fa592dbec4cce2d842288f711
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2022-06-19 17:03:21 +0900

    動いた

--- pycairo-examples/t14_hana.py
+++ pycairo-examples/t14_hana.py
@@ -4,17 +4,35 @@ import sys
 import scaled_font
 
 class Drawing:
-    def __init__(self, width, height):
-        # print('初期化してるよ', width, height)
+    def __init__(self, name, color, width, height):
+        print('初期化してるよ', width, height)
+        self.name = name
+        self.color = color
         self.width = width
         self.height = height
 
     def __str__(self):
-        return f'Drawing Object {self.width} {self.height}'
+        return f'Drawing Object {self.name} {self.color} {self.width} {self.height}'
+
+    def draw_frame(self):
+        print(f'Draw frames{self.color} {self.width} {self.height}')
+
+    def draw_circle_num(self):
+        print(f'Draw circle {self.width} {self.height}')
+
+    def draw_my_string(self):
+        print(f'Write word{self.width} {self.height}')
+
+    def draw_label(self):
+        self.draw_frame()
+        self.draw_circle_num()
+        self.draw_my_string ()
 
 if __name__ == '__main__':
-    dobj = Drawing(640, 480)
+    dobj = Drawing('hana', 'Purple', 640, 480)
     print(dobj)
+    dobj.draw_label()
 
-    dobj1 = Drawing(87, 30)
+    dobj1 = Drawing('anna', 'Blue', 87, 30)
     print(dobj1)
+    dobj1.draw_label()

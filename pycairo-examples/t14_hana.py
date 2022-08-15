7df4b69133b7cd9df0347e2e30bf4200c5caebd4
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2022-08-15 11:37:44 +0900

    カラー追加

--- pycairo-examples/t14_hana.py
+++ pycairo-examples/t14_hana.py
@@ -2,12 +2,14 @@ import cairo
 import math
 import sys
 import scaled_font
+import matplotlib.colors as mcolors
 
 class Label:
     def __init__(self, name):
         print('初期化してるよ')
         self.name = name
         self.color = 'blue'
+        self.line_width = 20
         self.width = 400
         self.height = 100
 
@@ -26,12 +28,29 @@ class Label:
         print('creating context')
 
     def save_image(self, filename):
-        print('ファイルネームにセーブするよ', filename)
+        print(f'{filename} にセーブします')
+        self.surface.write_to_png(filename)
 
     def __str__(self):
         return f'Drawing Object {self.name} {self.color} {self.width} {self.height}'
 
+    def set_source_color(self, name):
+        for colors in [mcolors.BASE_COLORS, mcolors.TABLEAU_COLORS, mcolors.CSS4_COLORS]:
+            for color_name, color in colors.items():
+                if name == color_name:
+                    #print('icchi', color)
+                    # #2E8B57
+                    if type(color) == tuple:
+                        new_color = color
+                    else:
+                        new_color = (int(color[1:3], 16)/255, int(color[3:5], 16)/255, int(color[5:7], 16)/255)
+                    self.context.set_source_rgb(new_color[0], new_color[1], new_color[2])
+                    break
+
     def draw_frame(self):
+        self.set_source_color(self.color)
+        self.context.set_line_width(self.line_width)
+
         print(f'Draw frames{self.color} {self.width} {self.height}')
 
     def draw_circle_num(self):
@@ -53,4 +72,4 @@ if __name__ == '__main__':
     mochan.init_context()
 
     mochan.draw_label()
-    mochan.save_image('mochan2.png')
+    mochan.save_image('MOCHAN.png')
